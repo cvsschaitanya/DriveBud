3f225f853a5bf9abeacd1e43e1762a79b389a2df
cvsschaitanya <cvsschaitanya@gmail.com>
2022-06-02 19:29:38 +0530

    changed format of stats

--- database.js
+++ database.js
@@ -1,10 +1,29 @@
-let stats = {};
-stats['KMs/unit'] = 6.0;
-stats['Total KM'] = 250.0;
-stats['Average Speed'] = 45.0;
-stats['Harsh Acceleration'] = 3.0;
-stats['Harsh Breaking Event'] = 4.0;
-stats['Driving Score'] = 7.5;
+let stats = [
+	{
+		statName: 'KMs/unit',
+		statValue: 6.0
+	},
+	{
+		statName: 'Total KM',
+		statValue: 250.0
+	},
+	{
+		statName: 'Average Speed',
+		statValue: 45.0
+	},
+	{
+		statName: 'Harsh Acceleration',
+		statValue: 3.0
+	},
+	{
+		statName: 'Harsh Breaking Event',
+		statValue: 4.0
+	},
+	{
+		statName: 'Driving Score',
+		statValue: 7.5
+	}
+];
 
 let home = {
 	battery: 93,
